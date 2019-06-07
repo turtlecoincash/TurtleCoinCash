Bigcountry907/turtlecoin

Switching to the argon2 implementation from https://github.com/WOnder93/argon2

external/argon2/arch/x86_64/lib/argon2-sse2.c
@@ -1,0 +1,124 @@
+#include "argon2-sse2.h"
+
+#ifdef HAVE_SSE2
+#include <x86intrin.h>
+
+#include "cpu-flags.h"
+
+#define ror64_16(x) \
+    _mm_shufflehi_epi16( \
+        _mm_shufflelo_epi16((x), _MM_SHUFFLE(0, 3, 2, 1)), \
+        _MM_SHUFFLE(0, 3, 2, 1))
+#define ror64_24(x) \
+    _mm_xor_si128(_mm_srli_epi64((x), 24), _mm_slli_epi64((x), 40))
+#define ror64_32(x) _mm_shuffle_epi32((x), _MM_SHUFFLE(2, 3, 0, 1))
+#define ror64_63(x) \
+    _mm_xor_si128(_mm_srli_epi64((x), 63), _mm_add_epi64((x), (x)))
+
+static __m128i f(__m128i x, __m128i y)
+{
+    __m128i z = _mm_mul_epu32(x, y);
+    return _mm_add_epi64(_mm_add_epi64(x, y), _mm_add_epi64(z, z));
+}
+
+#define G1(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        A0 = f(A0, B0); \
+        A1 = f(A1, B1); \
+\
+        D0 = _mm_xor_si128(D0, A0); \
+        D1 = _mm_xor_si128(D1, A1); \
+\
+        D0 = ror64_32(D0); \
+        D1 = ror64_32(D1); \
+\
+        C0 = f(C0, D0); \
+        C1 = f(C1, D1); \
+\
+        B0 = _mm_xor_si128(B0, C0); \
+        B1 = _mm_xor_si128(B1, C1); \
+\
+        B0 = ror64_24(B0); \
+        B1 = ror64_24(B1); \
+    } while ((void)0, 0)
+
+#define G2(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        A0 = f(A0, B0); \
+        A1 = f(A1, B1); \
+\
+        D0 = _mm_xor_si128(D0, A0); \
+        D1 = _mm_xor_si128(D1, A1); \
+\
+        D0 = ror64_16(D0); \
+        D1 = ror64_16(D1); \
+\
+        C0 = f(C0, D0); \
+        C1 = f(C1, D1); \
+\
+        B0 = _mm_xor_si128(B0, C0); \
+        B1 = _mm_xor_si128(B1, C1); \
+\
+        B0 = ror64_63(B0); \
+        B1 = ror64_63(B1); \
+    } while ((void)0, 0)
+
+#define DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        __m128i t0 = D0; \
+        __m128i t1 = B0; \
+        D0 = _mm_unpackhi_epi64(D1, _mm_unpacklo_epi64(t0, t0)); \
+        D1 = _mm_unpackhi_epi64(t0, _mm_unpacklo_epi64(D1, D1)); \
+        B0 = _mm_unpackhi_epi64(B0, _mm_unpacklo_epi64(B1, B1)); \
+        B1 = _mm_unpackhi_epi64(B1, _mm_unpacklo_epi64(t1, t1)); \
+    } while ((void)0, 0)
+
+#define UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        __m128i t0 = B0; \
+        __m128i t1 = D0; \
+        B0 = _mm_unpackhi_epi64(B1, _mm_unpacklo_epi64(B0, B0)); \
+        B1 = _mm_unpackhi_epi64(t0, _mm_unpacklo_epi64(B1, B1)); \
+        D0 = _mm_unpackhi_epi64(D0, _mm_unpacklo_epi64(D1, D1)); \
+        D1 = _mm_unpackhi_epi64(D1, _mm_unpacklo_epi64(t1, t1)); \
+    } while ((void)0, 0)
+
+#define BLAKE2_ROUND(A0, A1, B0, B1, C0, C1, D0, D1) \
+    do { \
+        G1(A0, B0, C0, D0, A1, B1, C1, D1); \
+        G2(A0, B0, C0, D0, A1, B1, C1, D1); \
+\
+        DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1); \
+\
+        G1(A0, B0, C1, D0, A1, B1, C0, D1); \
+        G2(A0, B0, C1, D0, A1, B1, C0, D1); \
+\
+        UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1); \
+    } while ((void)0, 0)
+
+#include "argon2-template-128.h"
+
+void fill_segment_sse2(const argon2_instance_t *instance,
+                       argon2_position_t position)
+{
+    fill_segment_128(instance, position);
+}
+
+int check_sse2(void)
+{
+    return cpu_flags_have_sse2();
+}
+
+#else
+
+void fill_segment_sse2(const argon2_instance_t *instance,
+                       argon2_position_t position)
+{
+}
+
+int check_sse2(void)
+{
+    return 0;
+}
+
+#endif

external/argon2/arch/x86_64/lib/argon2-ssse3.c
@@ -1,0 +1,136 @@
+#include "argon2-ssse3.h"
+
+#ifdef HAVE_SSSE3
+#include <string.h>
+
+#include <x86intrin.h>
+
+#include "cpu-flags.h"
+
+#define r16 (_mm_setr_epi8( \
+     2,  3,  4,  5,  6,  7,  0,  1, \
+    10, 11, 12, 13, 14, 15,  8,  9))
+
+#define r24 (_mm_setr_epi8( \
+     3,  4,  5,  6,  7,  0,  1,  2, \
+    11, 12, 13, 14, 15,  8,  9, 10))
+
+#define ror64_16(x) _mm_shuffle_epi8((x), r16)
+#define ror64_24(x) _mm_shuffle_epi8((x), r24)
+#define ror64_32(x) _mm_shuffle_epi32((x), _MM_SHUFFLE(2, 3, 0, 1))
+#define ror64_63(x) \
+    _mm_xor_si128(_mm_srli_epi64((x), 63), _mm_add_epi64((x), (x)))
+
+static __m128i f(__m128i x, __m128i y)
+{
+    __m128i z = _mm_mul_epu32(x, y);
+    return _mm_add_epi64(_mm_add_epi64(x, y), _mm_add_epi64(z, z));
+}
+
+#define G1(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        A0 = f(A0, B0); \
+        A1 = f(A1, B1); \
+\
+        D0 = _mm_xor_si128(D0, A0); \
+        D1 = _mm_xor_si128(D1, A1); \
+\
+        D0 = ror64_32(D0); \
+        D1 = ror64_32(D1); \
+\
+        C0 = f(C0, D0); \
+        C1 = f(C1, D1); \
+\
+        B0 = _mm_xor_si128(B0, C0); \
+        B1 = _mm_xor_si128(B1, C1); \
+\
+        B0 = ror64_24(B0); \
+        B1 = ror64_24(B1); \
+    } while ((void)0, 0)
+
+#define G2(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        A0 = f(A0, B0); \
+        A1 = f(A1, B1); \
+\
+        D0 = _mm_xor_si128(D0, A0); \
+        D1 = _mm_xor_si128(D1, A1); \
+\
+        D0 = ror64_16(D0); \
+        D1 = ror64_16(D1); \
+\
+        C0 = f(C0, D0); \
+        C1 = f(C1, D1); \
+\
+        B0 = _mm_xor_si128(B0, C0); \
+        B1 = _mm_xor_si128(B1, C1); \
+\
+        B0 = ror64_63(B0); \
+        B1 = ror64_63(B1); \
+    } while ((void)0, 0)
+
+#define DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        __m128i t0 = _mm_alignr_epi8(B1, B0, 8); \
+        __m128i t1 = _mm_alignr_epi8(B0, B1, 8); \
+        B0 = t0; \
+        B1 = t1; \
+\
+        t0 = _mm_alignr_epi8(D1, D0, 8); \
+        t1 = _mm_alignr_epi8(D0, D1, 8); \
+        D0 = t1; \
+        D1 = t0; \
+    } while ((void)0, 0)
+
+#define UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        __m128i t0 = _mm_alignr_epi8(B0, B1, 8); \
+        __m128i t1 = _mm_alignr_epi8(B1, B0, 8); \
+        B0 = t0; \
+        B1 = t1; \
+\
+        t0 = _mm_alignr_epi8(D0, D1, 8); \
+        t1 = _mm_alignr_epi8(D1, D0, 8); \
+        D0 = t1; \
+        D1 = t0; \
+    } while ((void)0, 0)
+
+#define BLAKE2_ROUND(A0, A1, B0, B1, C0, C1, D0, D1) \
+    do { \
+        G1(A0, B0, C0, D0, A1, B1, C1, D1); \
+        G2(A0, B0, C0, D0, A1, B1, C1, D1); \
+\
+        DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1); \
+\
+        G1(A0, B0, C1, D0, A1, B1, C0, D1); \
+        G2(A0, B0, C1, D0, A1, B1, C0, D1); \
+\
+        UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1); \
+    } while ((void)0, 0)
+
+#include "argon2-template-128.h"
+
+void fill_segment_ssse3(const argon2_instance_t *instance,
+                        argon2_position_t position)
+{
+    fill_segment_128(instance, position);
+}
+
+int check_ssse3(void)
+{
+    return cpu_flags_have_ssse3();
+}
+
+#else
+
+void fill_segment_ssse3(const argon2_instance_t *instance,
+                        argon2_position_t position)
+{
+}
+
+int check_ssse3(void)
+{
+    return 0;
+}
+
+#endif

external/argon2/arch/x86_64/lib/argon2-xop.c
@@ -1,0 +1,124 @@
+#include "argon2-xop.h"
+
+#ifdef HAVE_XOP
+#include <string.h>
+
+#include <x86intrin.h>
+
+#include "cpu-flags.h"
+
+#define ror64(x, c) _mm_roti_epi64((x), -(c))
+
+static __m128i f(__m128i x, __m128i y)
+{
+    __m128i z = _mm_mul_epu32(x, y);
+    return _mm_add_epi64(_mm_add_epi64(x, y), _mm_add_epi64(z, z));
+}
+
+#define G1(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        A0 = f(A0, B0); \
+        A1 = f(A1, B1); \
+\
+        D0 = _mm_xor_si128(D0, A0); \
+        D1 = _mm_xor_si128(D1, A1); \
+\
+        D0 = ror64(D0, 32); \
+        D1 = ror64(D1, 32); \
+\
+        C0 = f(C0, D0); \
+        C1 = f(C1, D1); \
+\
+        B0 = _mm_xor_si128(B0, C0); \
+        B1 = _mm_xor_si128(B1, C1); \
+\
+        B0 = ror64(B0, 24); \
+        B1 = ror64(B1, 24); \
+    } while ((void)0, 0)
+
+#define G2(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        A0 = f(A0, B0); \
+        A1 = f(A1, B1); \
+\
+        D0 = _mm_xor_si128(D0, A0); \
+        D1 = _mm_xor_si128(D1, A1); \
+\
+        D0 = ror64(D0, 16); \
+        D1 = ror64(D1, 16); \
+\
+        C0 = f(C0, D0); \
+        C1 = f(C1, D1); \
+\
+        B0 = _mm_xor_si128(B0, C0); \
+        B1 = _mm_xor_si128(B1, C1); \
+\
+        B0 = ror64(B0, 63); \
+        B1 = ror64(B1, 63); \
+    } while ((void)0, 0)
+
+#define DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        __m128i t0 = _mm_alignr_epi8(B1, B0, 8); \
+        __m128i t1 = _mm_alignr_epi8(B0, B1, 8); \
+        B0 = t0; \
+        B1 = t1; \
+\
+        t0 = _mm_alignr_epi8(D1, D0, 8); \
+        t1 = _mm_alignr_epi8(D0, D1, 8); \
+        D0 = t1; \
+        D1 = t0; \
+    } while ((void)0, 0)
+
+#define UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        __m128i t0 = _mm_alignr_epi8(B0, B1, 8); \
+        __m128i t1 = _mm_alignr_epi8(B1, B0, 8); \
+        B0 = t0; \
+        B1 = t1; \
+\
+        t0 = _mm_alignr_epi8(D0, D1, 8); \
+        t1 = _mm_alignr_epi8(D1, D0, 8); \
+        D0 = t1; \
+        D1 = t0; \
+    } while ((void)0, 0)
+
+#define BLAKE2_ROUND(A0, A1, B0, B1, C0, C1, D0, D1) \
+    do { \
+        G1(A0, B0, C0, D0, A1, B1, C1, D1); \
+        G2(A0, B0, C0, D0, A1, B1, C1, D1); \
+\
+        DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1); \
+\
+        G1(A0, B0, C1, D0, A1, B1, C0, D1); \
+        G2(A0, B0, C1, D0, A1, B1, C0, D1); \
+\
+        UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1); \
+    } while ((void)0, 0)
+
+#include "argon2-template-128.h"
+
+void fill_segment_xop(const argon2_instance_t *instance,
+                      argon2_position_t position)
+{
+    fill_segment_128(instance, position);
+}
+
+int check_xop(void)
+{
+    return cpu_flags_have_xop();
+}
+
+#else
+
+void fill_segment_xop(const argon2_instance_t *instance,
+                      argon2_position_t position)
+{
+}
+
+int check_xop(void)
+{
+    return 0;
+}
+
+#endif

turtlecoincash/TurtleCoinCash

external/argon2/arch/x86_64/lib/argon2-sse2.c
@@ -1,0 +1,122 @@
+#include "argon2-sse2.h"
+
+#ifdef HAVE_SSE2
+#include <immintrin.h>
+
+#define ror64_16(x) \
+    _mm_shufflehi_epi16( \
+        _mm_shufflelo_epi16((x), _MM_SHUFFLE(0, 3, 2, 1)), \
+        _MM_SHUFFLE(0, 3, 2, 1))
+#define ror64_24(x) \
+    _mm_xor_si128(_mm_srli_epi64((x), 24), _mm_slli_epi64((x), 40))
+#define ror64_32(x) _mm_shuffle_epi32((x), _MM_SHUFFLE(2, 3, 0, 1))
+#define ror64_63(x) \
+    _mm_xor_si128(_mm_srli_epi64((x), 63), _mm_add_epi64((x), (x)))
+
+static __m128i f(__m128i x, __m128i y)
+{
+    __m128i z = _mm_mul_epu32(x, y);
+    return _mm_add_epi64(_mm_add_epi64(x, y), _mm_add_epi64(z, z));
+}
+
+#define G1(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        A0 = f(A0, B0); \
+        A1 = f(A1, B1); \
+\
+        D0 = _mm_xor_si128(D0, A0); \
+        D1 = _mm_xor_si128(D1, A1); \
+\
+        D0 = ror64_32(D0); \
+        D1 = ror64_32(D1); \
+\
+        C0 = f(C0, D0); \
+        C1 = f(C1, D1); \
+\
+        B0 = _mm_xor_si128(B0, C0); \
+        B1 = _mm_xor_si128(B1, C1); \
+\
+        B0 = ror64_24(B0); \
+        B1 = ror64_24(B1); \
+    } while ((void)0, 0)
+
+#define G2(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        A0 = f(A0, B0); \
+        A1 = f(A1, B1); \
+\
+        D0 = _mm_xor_si128(D0, A0); \
+        D1 = _mm_xor_si128(D1, A1); \
+\
+        D0 = ror64_16(D0); \
+        D1 = ror64_16(D1); \
+\
+        C0 = f(C0, D0); \
+        C1 = f(C1, D1); \
+\
+        B0 = _mm_xor_si128(B0, C0); \
+        B1 = _mm_xor_si128(B1, C1); \
+\
+        B0 = ror64_63(B0); \
+        B1 = ror64_63(B1); \
+    } while ((void)0, 0)
+
+#define DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        __m128i t0 = D0; \
+        __m128i t1 = B0; \
+        D0 = _mm_unpackhi_epi64(D1, _mm_unpacklo_epi64(t0, t0)); \
+        D1 = _mm_unpackhi_epi64(t0, _mm_unpacklo_epi64(D1, D1)); \
+        B0 = _mm_unpackhi_epi64(B0, _mm_unpacklo_epi64(B1, B1)); \
+        B1 = _mm_unpackhi_epi64(B1, _mm_unpacklo_epi64(t1, t1)); \
+    } while ((void)0, 0)
+
+#define UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        __m128i t0 = B0; \
+        __m128i t1 = D0; \
+        B0 = _mm_unpackhi_epi64(B1, _mm_unpacklo_epi64(B0, B0)); \
+        B1 = _mm_unpackhi_epi64(t0, _mm_unpacklo_epi64(B1, B1)); \
+        D0 = _mm_unpackhi_epi64(D0, _mm_unpacklo_epi64(D1, D1)); \
+        D1 = _mm_unpackhi_epi64(D1, _mm_unpacklo_epi64(t1, t1)); \
+    } while ((void)0, 0)
+
+#define BLAKE2_ROUND(A0, A1, B0, B1, C0, C1, D0, D1) \
+    do { \
+        G1(A0, B0, C0, D0, A1, B1, C1, D1); \
+        G2(A0, B0, C0, D0, A1, B1, C1, D1); \
+\
+        DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1); \
+\
+        G1(A0, B0, C1, D0, A1, B1, C0, D1); \
+        G2(A0, B0, C1, D0, A1, B1, C0, D1); \
+\
+        UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1); \
+    } while ((void)0, 0)
+
+#include "argon2-template-128.h"
+
+void fill_segment_sse2(const argon2_instance_t *instance,
+                       argon2_position_t position)
+{
+    fill_segment_128(instance, position);
+}
+
+int check_sse2(void)
+{
+    return 1;
+}
+
+#else
+
+void fill_segment_sse2(const argon2_instance_t *instance,
+                       argon2_position_t position)
+{
+}
+
+int check_sse2(void)
+{
+    return 0;
+}
+
+#endif

external/argon2/arch/x86_64/lib/argon2-ssse3.c
@@ -1,0 +1,134 @@
+#include "argon2-ssse3.h"
+
+#ifdef HAVE_SSSE3
+#include <string.h>
+
+#include <immintrin.h>
+
+#define r16 (_mm_setr_epi8( \
+     2,  3,  4,  5,  6,  7,  0,  1, \
+    10, 11, 12, 13, 14, 15,  8,  9))
+
+#define r24 (_mm_setr_epi8( \
+     3,  4,  5,  6,  7,  0,  1,  2, \
+    11, 12, 13, 14, 15,  8,  9, 10))
+
+#define ror64_16(x) _mm_shuffle_epi8((x), r16)
+#define ror64_24(x) _mm_shuffle_epi8((x), r24)
+#define ror64_32(x) _mm_shuffle_epi32((x), _MM_SHUFFLE(2, 3, 0, 1))
+#define ror64_63(x) \
+    _mm_xor_si128(_mm_srli_epi64((x), 63), _mm_add_epi64((x), (x)))
+
+static __m128i f(__m128i x, __m128i y)
+{
+    __m128i z = _mm_mul_epu32(x, y);
+    return _mm_add_epi64(_mm_add_epi64(x, y), _mm_add_epi64(z, z));
+}
+
+#define G1(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        A0 = f(A0, B0); \
+        A1 = f(A1, B1); \
+\
+        D0 = _mm_xor_si128(D0, A0); \
+        D1 = _mm_xor_si128(D1, A1); \
+\
+        D0 = ror64_32(D0); \
+        D1 = ror64_32(D1); \
+\
+        C0 = f(C0, D0); \
+        C1 = f(C1, D1); \
+\
+        B0 = _mm_xor_si128(B0, C0); \
+        B1 = _mm_xor_si128(B1, C1); \
+\
+        B0 = ror64_24(B0); \
+        B1 = ror64_24(B1); \
+    } while ((void)0, 0)
+
+#define G2(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        A0 = f(A0, B0); \
+        A1 = f(A1, B1); \
+\
+        D0 = _mm_xor_si128(D0, A0); \
+        D1 = _mm_xor_si128(D1, A1); \
+\
+        D0 = ror64_16(D0); \
+        D1 = ror64_16(D1); \
+\
+        C0 = f(C0, D0); \
+        C1 = f(C1, D1); \
+\
+        B0 = _mm_xor_si128(B0, C0); \
+        B1 = _mm_xor_si128(B1, C1); \
+\
+        B0 = ror64_63(B0); \
+        B1 = ror64_63(B1); \
+    } while ((void)0, 0)
+
+#define DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        __m128i t0 = _mm_alignr_epi8(B1, B0, 8); \
+        __m128i t1 = _mm_alignr_epi8(B0, B1, 8); \
+        B0 = t0; \
+        B1 = t1; \
+\
+        t0 = _mm_alignr_epi8(D1, D0, 8); \
+        t1 = _mm_alignr_epi8(D0, D1, 8); \
+        D0 = t1; \
+        D1 = t0; \
+    } while ((void)0, 0)
+
+#define UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        __m128i t0 = _mm_alignr_epi8(B0, B1, 8); \
+        __m128i t1 = _mm_alignr_epi8(B1, B0, 8); \
+        B0 = t0; \
+        B1 = t1; \
+\
+        t0 = _mm_alignr_epi8(D0, D1, 8); \
+        t1 = _mm_alignr_epi8(D1, D0, 8); \
+        D0 = t1; \
+        D1 = t0; \
+    } while ((void)0, 0)
+
+#define BLAKE2_ROUND(A0, A1, B0, B1, C0, C1, D0, D1) \
+    do { \
+        G1(A0, B0, C0, D0, A1, B1, C1, D1); \
+        G2(A0, B0, C0, D0, A1, B1, C1, D1); \
+\
+        DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1); \
+\
+        G1(A0, B0, C1, D0, A1, B1, C0, D1); \
+        G2(A0, B0, C1, D0, A1, B1, C0, D1); \
+\
+        UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1); \
+    } while ((void)0, 0)
+
+#include "argon2-template-128.h"
+
+void fill_segment_ssse3(const argon2_instance_t *instance,
+                        argon2_position_t position)
+{
+    fill_segment_128(instance, position);
+}
+
+int check_ssse3(void)
+{
+    return 1;
+}
+
+#else
+
+void fill_segment_ssse3(const argon2_instance_t *instance,
+                        argon2_position_t position)
+{
+}
+
+int check_ssse3(void)
+{
+    return 0;
+}
+
+#endif

external/argon2/arch/x86_64/lib/argon2-xop.c
@@ -1,0 +1,122 @@
+#include "argon2-xop.h"
+
+#ifdef HAVE_XOP
+#include <string.h>
+
+#include <immintrin.h>
+
+#define ror64(x, c) _mm_roti_epi64((x), -(c))
+
+static __m128i f(__m128i x, __m128i y)
+{
+    __m128i z = _mm_mul_epu32(x, y);
+    return _mm_add_epi64(_mm_add_epi64(x, y), _mm_add_epi64(z, z));
+}
+
+#define G1(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        A0 = f(A0, B0); \
+        A1 = f(A1, B1); \
+\
+        D0 = _mm_xor_si128(D0, A0); \
+        D1 = _mm_xor_si128(D1, A1); \
+\
+        D0 = ror64(D0, 32); \
+        D1 = ror64(D1, 32); \
+\
+        C0 = f(C0, D0); \
+        C1 = f(C1, D1); \
+\
+        B0 = _mm_xor_si128(B0, C0); \
+        B1 = _mm_xor_si128(B1, C1); \
+\
+        B0 = ror64(B0, 24); \
+        B1 = ror64(B1, 24); \
+    } while ((void)0, 0)
+
+#define G2(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        A0 = f(A0, B0); \
+        A1 = f(A1, B1); \
+\
+        D0 = _mm_xor_si128(D0, A0); \
+        D1 = _mm_xor_si128(D1, A1); \
+\
+        D0 = ror64(D0, 16); \
+        D1 = ror64(D1, 16); \
+\
+        C0 = f(C0, D0); \
+        C1 = f(C1, D1); \
+\
+        B0 = _mm_xor_si128(B0, C0); \
+        B1 = _mm_xor_si128(B1, C1); \
+\
+        B0 = ror64(B0, 63); \
+        B1 = ror64(B1, 63); \
+    } while ((void)0, 0)
+
+#define DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        __m128i t0 = _mm_alignr_epi8(B1, B0, 8); \
+        __m128i t1 = _mm_alignr_epi8(B0, B1, 8); \
+        B0 = t0; \
+        B1 = t1; \
+\
+        t0 = _mm_alignr_epi8(D1, D0, 8); \
+        t1 = _mm_alignr_epi8(D0, D1, 8); \
+        D0 = t1; \
+        D1 = t0; \
+    } while ((void)0, 0)
+
+#define UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1) \
+    do { \
+        __m128i t0 = _mm_alignr_epi8(B0, B1, 8); \
+        __m128i t1 = _mm_alignr_epi8(B1, B0, 8); \
+        B0 = t0; \
+        B1 = t1; \
+\
+        t0 = _mm_alignr_epi8(D0, D1, 8); \
+        t1 = _mm_alignr_epi8(D1, D0, 8); \
+        D0 = t1; \
+        D1 = t0; \
+    } while ((void)0, 0)
+
+#define BLAKE2_ROUND(A0, A1, B0, B1, C0, C1, D0, D1) \
+    do { \
+        G1(A0, B0, C0, D0, A1, B1, C1, D1); \
+        G2(A0, B0, C0, D0, A1, B1, C1, D1); \
+\
+        DIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1); \
+\
+        G1(A0, B0, C1, D0, A1, B1, C0, D1); \
+        G2(A0, B0, C1, D0, A1, B1, C0, D1); \
+\
+        UNDIAGONALIZE(A0, B0, C0, D0, A1, B1, C1, D1); \
+    } while ((void)0, 0)
+
+#include "argon2-template-128.h"
+
+void fill_segment_xop(const argon2_instance_t *instance,
+                      argon2_position_t position)
+{
+    fill_segment_128(instance, position);
+}
+
+int check_xop(void)
+{
+    return 1;
+}
+
+#else
+
+void fill_segment_xop(const argon2_instance_t *instance,
+                      argon2_position_t position)
+{
+}
+
+int check_xop(void)
+{
+    return 0;
+}
+
+#endif